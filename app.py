085800c4335cf0ef2c2eebc699c2eeede88181c5
mktia <endoplasmicreticulum2468741059@gmail.com>
2017-03-24 01:55:37 +0900

    add functions

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 import urllib2
+import time
 import tweepy
 from flask import Flask, render_template, session, request, redirect
 
@@ -20,13 +21,27 @@ setting = {
 
 callback_url = setting['url']
 user = {}
+twitter = {}
+res = {}
 
-def verify():
+def check_sess():
+	if 'name' in session:
+		pass
+	else:
+		print('red')
+		redirect(setting['url'])
+
+def verify(request_token):
 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret, callback_url)
-	auth.request_token = session.pop('request_token')
+	auth.request_token = session.pop(request_token)
 	auth.get_access_token(session.pop('verifier'))
 	auth.set_access_token(auth.access_token, auth.access_token_secret)
 	return(tweepy.API(auth))
+	
+def xauth_verify(key):
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret, callback_url + '/' + key)
+	twitter['redirect_' + key] = auth.get_authorization_url(signin_with_twitter=True)
+	return(auth.request_token)
 
 def get_profile_image(obj):
 	"""
@@ -36,20 +51,31 @@ def get_profile_image(obj):
 		profile_img = obj.profile_image_url
 		profile_img = (obj.profile_image_url[:profile_img.rfind('_')]
 			+ obj.profile_image_url[profile_img.rfind('.'):])
-		return profile_img
+		return(profile_img)
 	except:
-		return obj.profile_image_url
-
+		return(obj.profile_image_url)
+		
+def make_list(api, input_list, output_dict):
+	start = time.time()
+	for i in range(0, len(input_list[:]), 100):
+		if(time.time() - start > 14):
+			return(True)
+		for user in api.lookup_users(input_list[i:i+100]):
+			output_dict['screen_name'].append(user.screen_name)
+			output_dict['name'].append(user.name)
+			output_dict['icon'].append(get_profile_image(user))
+	return(False)
+			
 @app.route('/')
 def top():
 	if 'name' in session:
-		auth = tweepy.OAuthHandler(consumer_key, consumer_secret, callback_url + '/ff')
-		redirect_url_ff = auth.get_authorization_url(signin_with_twitter=True)
-		session['request_token'] = auth.request_token
+		session['request_token_not_friend'] = xauth_verify('not_friend')
+		session['request_token_not_follow'] = xauth_verify('not_follow')
+		session['request_token_ff'] = xauth_verify('ff')
 		user['name'] = session.get('name')
 		user['screen_name'] = session.get('screen_name')
 		user['icon'] = session.get('icon')
-		return(render_template('main.html', info=setting, user=user, re_text=redirect_url_ff))
+		return(render_template('main.html', info=setting, user=user, twitter=twitter))
 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret, callback_url + '/login')
 	redirect_url = auth.get_authorization_url()
 	session['request_token'] = auth.request_token
@@ -57,76 +83,128 @@ def top():
 
 @app.route('/login')
 def oauth_login():
-	session['verifier'] = request.args.get('oauth_verifier')
-	api = verify()
-	
-	owner = api.me()
-	session['name'] = owner.name
-	session['screen_name'] = owner.screen_name
-	session['icon'] = get_profile_image(owner)
+	if 'request_token' in session:
+		session['verifier'] = request.args.get('oauth_verifier')
+		api = verify('request_token')
+		
+		owner = api.me()
+		session['name'] = owner.name
+		session['screen_name'] = owner.screen_name
+		session['icon'] = get_profile_image(owner)
+		return(redirect(setting['url']))
 	return(redirect(setting['url']))
+	
+@app.route('/not_friend')
+def not_fr_check():
+	st = time.time()
+	if 'name' in session:
+		session['verifier'] = request.args.get('oauth_verifier')
+		api = verify('request_token_not_friend')
+	
+		friends = []
+		followers = []
+		
+		for fr in tweepy.Cursor(api.friends_ids).items():
+			friends.append(fr)
+		for fo in tweepy.Cursor(api.followers_ids).items():
+			followers.append(fo)
 
+		not_fr_id = [] # The ID list of account you aren't following.
+		not_fr = {'name':[], 'screen_name':[], 'icon':[]}
+		
+		for fo in followers:
+			for fr in friends:
+				if fo == fr:
+					break
+			else:
+				not_fr_id.append(fo)
+				
+		overtime = make_list(api, not_fr_id, not_fr)
+		res['title'] = u'フォローを返していないフォロワー'
+		res['length'] = len(not_fr['screen_name'])
+		res['message'] = u'あなたがフォローを返していないフォロワーはいませんでした。'
+		
+		print(time.time() - st)
+		
+		return render_template('result.html', info=setting,	user=user, list=not_fr, res=res, overtime=overtime)
+	return(redirect(setting['url']))
+		
+@app.route('/not_follow')
+def not_fo_check():
+	st = time.time()
+	if 'name' in session:
+		session['verifier'] = request.args.get('oauth_verifier')
+		api = verify('request_token_not_follow')
+		
+		friends = []
+		followers = []
+		
+		for fr in tweepy.Cursor(api.friends_ids).items():
+			friends.append(fr)
+		for fo in tweepy.Cursor(api.followers_ids).items():
+			followers.append(fo)
+		
+		not_fo_id = [] # The ID list of account you aren't followed by.
+		not_fo = {'name':[], 'screen_name':[], 'icon':[]}
+		
+		for fr in friends:
+			for fo in followers:
+				if fr == fo:
+					break
+			else:
+				not_fo_id.append(fr)
+
+		overtime = make_list(api, not_fo_id, not_fo)
+		res['title'] = u'あなたをフォローしていないフォロワー'
+		res['length'] = len(not_fo['screen_name'])
+		res['message'] = u'あなたをフォローしていないフォロワーはいませんでした。'
+		
+		print(time.time() - st)
+		
+		return render_template('result.html', info=setting,	user=user, list=not_fo, res=res, overtime=overtime)
+	return(redirect(setting['url']))
+		
 @app.route('/ff')
 def ff_check():
-	def make_list(input_list, output_dict):
-		for i in range(0, len(input_list[:]), 100):
-			for user in api.lookup_users(input_list[i:i+100]):
-				output_dict['screen_name'].append(user.screen_name)
-				output_dict['name'].append(user.name)
-				output_dict['icon'].append(get_profile_image(user))
-	
-	session['verifier'] = request.args.get('oauth_verifier')
-	api = verify()
-	
-	length = {}
-	friends = []
-	followers = []
-	
-	for fr in tweepy.Cursor(api.friends_ids).items():
-		friends.append(fr)
-	for fo in tweepy.Cursor(api.followers_ids).items():
-		followers.append(fo)
-	
-	print('follows: %d, followers: %d' % (len(friends), len(followers)))
-	
-	fr_and_fo_id = []
-	not_fr_id = [] # The ID list of account you aren't following.
-	not_fo_id = [] # The ID list of account you aren't followed by.
-	
-	fr_and_fo = {'name':[], 'screen_name':[], 'icon':[]}
-	not_fr = {'name':[], 'screen_name':[], 'icon':[]}
-	not_fo = {'name':[], 'screen_name':[], 'icon':[]}
-	
-	for fr in friends:
-		for fo in followers:
-			if fr == fo:
-				fr_and_fo_id.append(fr)
-				break
-		else:
-			not_fo_id.append(fr)
-	for fo in followers:
+	st = time.time()
+	if 'name' in session:
+		session['verifier'] = request.args.get('oauth_verifier')
+		api = verify('request_token_ff')
+		
+		friends = []
+		followers = []
+		
+		for fr in tweepy.Cursor(api.friends_ids).items():
+			friends.append(fr)
+		for fo in tweepy.Cursor(api.followers_ids).items():
+			followers.append(fo)
+		
+		fr_and_fo_id = []	
+		fr_and_fo = {'name':[], 'screen_name':[], 'icon':[]}
+		
 		for fr in friends:
-			if fo == fr:
-				break
-		else:
-			not_fr_id.append(fo)
+			for fo in followers:
+				if fr == fo:
+					fr_and_fo_id.append(fr)
+					break
 
-	make_list(fr_and_fo_id, fr_and_fo)
-	make_list(not_fr_id, not_fr)
-	make_list(not_fo_id, not_fo)
+		overtime = make_list(api, fr_and_fo_id, fr_and_fo)
+		res['title'] = u'相互フォローのユーザー'
+		res['length'] = len(fr_and_fo['screen_name'])
+		res['message'] = u'互いにフォローしているユーザーはいませんでした。'
+		
+		print(time.time() - st)
+		
+		return render_template('result.html', info=setting,	user=user, list=fr_and_fo, res=res, overtime=overtime)
+	return(redirect(setting['url']))
+
+@app.route('/about')
+def about():
+	return(render_template('about.html', info=setting))
 	
-	length['num_fr_and_fo'] = len(fr_and_fo['screen_name'])
-	length['num_not_fo'] = len(not_fo['screen_name'])
-	length['num_not_fr'] = len(not_fr['screen_name'])
-	print('ff: %d, not_fo: %d, not_fr: %d'
-		% (length['num_fr_and_fo'], length['num_not_fo'], length['num_not_fr'])) 
-	
-	return render_template('result.html', info=setting,
-		fr_and_fo=fr_and_fo, not_fo=not_fo, not_fr=not_fr, length=length)
-
-#@app.route('/contact')
-
-#top version
+@app.route('/version')
+def version():
+	return(render_template('version.html', info=setting))
 
 if __name__ == '__main__':
 	app.run()
